adbca127d6bdb0771ed5b31499b2d063abcc796f
rashiniashinsana <rashinivithanage2@gmail.com>
2024-08-21 23:48:14 +0530

    Create User DTO and User

---
+++ src/main/java/lk/ijse/gdse68/jwt/entity/User.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse68.jwt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "systemuser")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uid;
+    private String email;
+    private String password;
+    private String name;
+    private String companyName;
+    private String role;
+
+}
